d89c77f068511791d7dfbdabaad2f7433c6fe6bc
Fikrat Mammadov <fikrat.mammadov.a@asoiu.edu.az>
2021-12-03 22:25:21 +0400

    Add project files.

---
+++ BusinessLayer/Concrete/CategoryManager.cs
@@ -0,0 +1,44 @@
+﻿using BusinessLayer.Abstract;
+using DataAccessLayer.EntityFramework;
+using EntityLayer.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLayer.Concrete
+{
+    public class CategoryManager : ICategoryService
+    {
+        EfCategoryRepository repository;
+        public CategoryManager()
+        {
+            repository = new EfCategoryRepository();
+        }
+        public void Add(Category category)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Delete(Category category)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Category> GetAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Category GetById(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Update(Category category)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ DataAccessLayer/Repositories/CategoryRepository.cs
@@ -0,0 +1,57 @@
+﻿using DataAccessLayer.Abstract;
+using DataAccessLayer.Concrete;
+using EntityLayer.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccessLayer.Repositories
+{
+    public class CategoryRepository : ICategoryDal
+    {
+        public void Add(Category category)
+        {
+            using (var context = new Context())
+            {
+                context.Add(category);
+                context.SaveChanges();
+            }
+        }
+
+        public void Delete(Category category)
+        {
+            using (var context = new Context())
+            {
+                context.Remove(category);
+                context.SaveChanges();
+            }
+        }
+
+        public List<Category> GetAll()
+        {
+            using (var context = new Context())
+            {
+                return context.Categories.ToList();
+            }
+        }
+
+        public Category GetById(int id)
+        {
+            using (var context = new Context())
+            {
+                return context.Categories.SingleOrDefault(c => c.Id == id);
+            }
+        }
+
+        public void Update(Category category)
+        {
+            using (var context = new Context())
+            {
+                context.Update(category);
+                context.SaveChanges();
+            }
+        }
+    }
+}
